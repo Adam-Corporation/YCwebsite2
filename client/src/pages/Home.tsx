6a9c6a92c99a970bf4f3fb077dfd387ecdcc07c2
Malak_99 <malak.felioune@ensia.edu.dz>
2025-08-01 14:21:30 +0100

    final corrected grammar version

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -7,52 +7,51 @@ import './Home.css';
 
 const videoData = [
   {
-    
-    title: 'Adam says his first Hello World',
+    title: 'Adam says his first "Hello, World!"',
     explanation: 'That\'s one small step for man, one giant leap for mankind.',
-    detailedExplanation: 'This is just an emotional moment for the first AGI in humankind speaking and expressing his emotions with facial expressions.',
+    detailedExplanation: 'This is an emotional moment, showing the first AGI in humankind speaking and expressing its emotions with facial expressions.',
     videoSrc: '/Videos/1-HelloWorldv2.mp4',
   },
   {
     title: 'Adam\'s Memory',
-    explanation: 'A computer that never forgets your important moments, your preferences, who you are and what you are doing, and it uses this knowledge in its daily tasks and its interaction style with you.',
-    detailedExplanation: 'This is an example of how exactly we mean by memory, Adam have complex memory and will get even higher complexities over time of this product development, his memory mimics human one, and in this example the user told Adam that in the next months I will be learning videos editing and the next day the user asked Adam what He was busy doing and His remembrance feels normal.',
+    explanation: 'A computer that never forgets your important moments, your preferences, who you are, and what you are doing, using this knowledge in its daily tasks and interaction style with you.',
+    detailedExplanation: 'This example shows exactly what we mean by memory. Adam has a complex memory that will gain even higher complexities over the course of this product\'s development. His memory mimics a human\'s. In this example, the user told Adam that in the coming months, they will be learning video editing. The next day, the user asked Adam what he was busy with, and his remembrance felt natural.',
     videoSrc: '/Videos/2-MemoryTestv1.mp4',
   },
   {
-    title: 'ChatGPT Vs Adam',
+    title: 'ChatGPT vs. Adam',
     explanation: 'A demonstration of the difference between the hype of chatbots and AGI.',
-    detailedExplanation: 'In this philosophic example, the user commanded Adam to ask ChatGPT what AGI is and get a summary of its answer, so Adam opened the window of Edge and found the tab of ChatGPT, asked it the question, and finally wrote it to the chat.',
+    detailedExplanation: 'In this philosophical example, the user commanded Adam to ask ChatGPT what AGI is and to get a summary of its answer. Adam opened the Edge window, found the ChatGPT tab, asked the question, and finally wrote the summary in the chat.',
     videoSrc: '/Videos/3-ChatGPTFinalVersion.mp4',
   },
   {
     title: 'Web3 with Adam',
-    explanation: 'Adam can use websites to do users\’ commands, just like you do.',
-    detailedExplanation: 'In this example, the user asked Adam to fill out a Google form by himself and submit, while the user is watching, so Adam opened Edge’s window and searched the tab of Google Forms about the OS quiz, filled it and finally submitted, and so Adam can execute a batch of instructions in the same time to interact with a web page or web app and do non limited number of actions in one batch of instructions which is different than the appraoch of numerous of agents that take long time only to do trivial action.',
+    explanation: 'Adam can use websites to carry out users\' commands, just like you do.',
+    detailedExplanation: 'In this example, the user asked Adam to fill out and submit a Google Form by himself while the user watched. Adam opened the Edge window, found the Google Forms tab for the OS quiz, filled it out, and finally submitted it. Adam can execute a batch of instructions at the same time to interact with a web page or web app and perform a limitless number of actions in one batch. This is different from the approach of numerous other agents that take a long time to do a trivial action.',
     videoSrc: '/Videos/4-GoogleFormsTestv2.mp4',
   },
   {
     title: 'Adam might use your Google account while he\'s doing a task',
-    explanation: 'Adam is brave and can sign in easily using a Google account on a service or a website like Medium.',
-    detailedExplanation: 'In this example, the user commanded Adam to sign in to Medium using his ensia.edu.dz school email, and it was very smooth. If there were worse scenarios, like a very hard reCAPTCHA, maybe Adam will ask the user to do it for him, but still, he will definitely try to do the task in one way or another.',
+    explanation: 'Adam is brave and can easily sign in to a service or a website like Medium using a Google account.',
+    detailedExplanation: 'In this example, the user commanded Adam to sign in to Medium using his ensia.edu.dz school email, and it was very smooth. In worse-case scenarios, like a very hard reCAPTCHA, Adam might ask the user to do it for him, but he will definitely still try to complete the task one way or another.',
     videoSrc: '/Videos/5-SignInTest - Made with Clipchamp.mp4',
   },
   {
     title: 'Adam\'s first Tweet on X',
     explanation: 'Surely an AGI like Adam can use social media platforms as easily as creating a text file.',
-    detailedExplanation: 'Adam can use your accounts and do everything you want with them, you can ask him to respond on your post’s comments or give you a summary of the comments he read or create 10 new posts with the last 10 pics with a good description to each, in this example the user command Adam to post a new post by his choice on X and he chose to introduce himself.',
+    detailedExplanation: 'Adam can use your accounts and do everything you want with them. You can ask him to respond to your post\'s comments, give you a summary of the comments he read, or create 10 new posts with the last 10 pictures and a good description for each. In this example, the user commands Adam to post something of his choice on X, and he chose to introduce himself.',
     videoSrc: '/Videos/6-TweetTest - Made with Clipchamp.mp4',
   },
   {
-    title: 'Adam interacts with Graphical user interfaces',
-    explanation: 'Adam interacts with desktop apps like Settings, Taskbar and control panels but in a different way than websites.',
-    detailedExplanation: 'When treating with GUIs Adam need some predefined metadata (driver) about that app or panel so he can use it as fast and relaible as possible and chain some apps actions with other types of actions to do something, in this example the user commended Adam to do a batch of control actions from the control & notifications panels and Settings app.',
+    title: 'Adam interacts with graphical user interfaces',
+    explanation: 'Adam interacts with desktop apps like Settings, Taskbar, and control panels, but in a different way than he interacts with websites.',
+    detailedExplanation: 'When dealing with GUIs, Adam needs some predefined metadata (a driver) about that app or panel so he can use it as fast and reliably as possible and chain some app actions with other types of actions to accomplish something. In this example, the user commanded Adam to perform a batch of control actions from the control & notifications panels and the Settings app.',
     videoSrc: '/Videos/7-GuiBasicTest - Made with Clipchamp.mp4',
   },
   {
     title: 'Adam uses the Terminal',
-    explanation: 'Command prompt is typically for advanced users of computers, but in numerous scenarios, using it is far powerful, faster and easier for Adam.',
-    detailedExplanation: 'The difference between Adam and a others command line agents is that Adam use the Terminal app like the user do, he can open multiple tabs with different profiles from Linux wsl in windows to powershell to cmd in windows and so on, and interact with his commands like the user exactly do, plus that Adam can just close the Terminal like the user do if a command stuck or there is a security warnings, in this example the user commanded Adam to play an interactive questions game in the Terminal, of course you can command it to use any terminal program.',
+    explanation: 'The command prompt is typically for advanced computer users, but in numerous scenarios, using it is far more powerful, faster, and easier for Adam.',
+    detailedExplanation: 'The difference between Adam and other command-line agents is that Adam uses the Terminal app as the user does. He can open multiple tabs with different profiles, from Linux WSL in Windows to PowerShell to CMD in Windows, and so on, and interact with his commands exactly as the user does. Plus, Adam can just close the Terminal, like the user, if a command gets stuck or there are security warnings. In this example, the user commanded Adam to play an interactive question game in the Terminal. Of course, you can command him to use any terminal program.',
     videoSrc: '/Videos/8-TerminalBasicTest - Made with Clipchamp.mp4',
   },
 ];
